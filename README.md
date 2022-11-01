b9d8de1b6d4e6e033d4ffa413c8c962163b3e264
Artem Vasilev <kern.usr@gmail.com>
2022-11-01 16:04:18 +0300

    Команда

---
+++ README.md
@@ -0,0 +1,1 @@
+# tbc-remove-pinned-messages
---
+++ src/Commands/SystemCommands/RemoveVoiceVideoChatMessagesCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Webmasterskaya\TelegramBotCommands\Commands\SystemCommands;
+
+use Longman\TelegramBot\Commands\SystemCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Request;
+
+class RemoveVoiceVideoChatMessagesCommand extends SystemCommand
+{
+	/**
+	 * @var string
+	 */
+	protected $name = 'removevoicevideochatmessages';
+
+	/**
+	 * @var string
+	 */
+	protected $description = 'Handles and remove system messages about starting and ending of video and voice chat';
+
+	/**
+	 * @var string
+	 */
+	protected $version = '1.0.0';
+
+	public function execute(): ServerResponse
+	{
+		$message = $this->getMessage() ?: $this->getEditedMessage() ?: $this->getChannelPost() ?: $this->getEditedChannelPost();
+
+		if ($message->getVideoChatStarted()
+			|| $message->getVideoChatEnded()
+			|| $message->getVoiceChatStarted()
+			|| $message->getVoiceChatEnded())
+		{
+			return Request::deleteMessage([
+				'chat_id'    => $message->getChat()->getId(),
+				'message_id' => $message->getMessageId()
+			]);
+		}
+
+		return Request::emptyResponse();
+	}
+}
